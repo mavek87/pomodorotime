9a3a181bbfdf85682049d6b3232127f89d5cd5b8
mavek87 <matver87@gmail.com>
2022-05-04 20:44:54 +0200

    simplified code

--- src/main/java/com/matteoveroni/pomodorotime/singleton/ConfigSingleton.java
+++ src/main/java/com/matteoveroni/pomodorotime/singleton/ConfigSingleton.java
@@ -1,7 +1,6 @@
 package com.matteoveroni.pomodorotime.singleton;
 
 import com.matteoveroni.pomodorotime.configs.Config;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,9 +12,8 @@ public enum ConfigSingleton {
 
     ConfigSingleton() {
         try {
-            Logger log = LoggerFactory.getLogger(ConfigSingleton.class);
-            Path configPath = ExternalFolderPathSingleton.INSTANCE.getPath("config", "config.json");
-            log.info("config path: {}", configPath);
+            final Path configPath = ExternalFolderPathSingleton.INSTANCE.getPath("config", "config.json");
+            LoggerFactory.getLogger(ConfigSingleton.class).info("config path: {}", configPath);
             this.config = GsonSingleton.INSTANCE.getGson().fromJson(Files.readString(configPath), Config.class);
         } catch (Exception ex) {
             throw new RuntimeException("Error", ex);
