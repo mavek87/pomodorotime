b84e1a816b7d9b060c3d7c990a159d02ed371e33
mavek87 <matver87@gmail.com>
2022-05-13 09:39:45 +0200

    add logs in JsonConfigManager

--- src/main/java/com/matteoveroni/pomodorotime/configs/JsonConfigManager.java
+++ src/main/java/com/matteoveroni/pomodorotime/configs/JsonConfigManager.java
@@ -3,12 +3,14 @@ package com.matteoveroni.pomodorotime.configs;
 import com.google.gson.Gson;
 import com.matteoveroni.pomodorotime.utils.singleton.ExternalFolderPathSingleton;
 import com.matteoveroni.pomodorotime.utils.singleton.GsonSingleton;
+import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.Semaphore;
 
+@Slf4j
 public enum JsonConfigManager implements ConfigManager {
     INSTANCE;
 
@@ -23,6 +25,7 @@ public enum JsonConfigManager implements ConfigManager {
             configFileSemaphore.acquireUninterruptibly();
             return gson.fromJson(Files.readString(configPath), Config.class);
         } catch (IOException ex) {
+            log.error("Error", ex);
             throw new RuntimeException("Error trying to read from config file", ex);
         } finally {
             configFileSemaphore.release();
@@ -33,8 +36,11 @@ public enum JsonConfigManager implements ConfigManager {
     public void writeConfig(Config config) {
         try {
             configFileSemaphore.acquireUninterruptibly();
-            Files.writeString(configPath, gson.toJson(config), StandardOpenOption.TRUNCATE_EXISTING);
+            final String jsonConfig = gson.toJson(config);
+            log.debug("Writing to file: {} config:\n{}", configPath.toAbsolutePath(), jsonConfig);
+            Files.writeString(configPath, jsonConfig, StandardOpenOption.TRUNCATE_EXISTING);
         } catch (IOException ex) {
+            log.error("Error", ex);
             throw new RuntimeException("Error trying to write to config file");
         } finally {
             configFileSemaphore.release();
